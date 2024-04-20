864b012804f99917f214794a37d09900cc954197
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-20 19:41:41 -0300

    .

--- src/main/java/com/ccp/implementations/password/mindrot/CcpMindrotPasswordHandler.java
+++ src/main/java/com/ccp/implementations/password/mindrot/CcpMindrotPasswordHandler.java
@@ -1,12 +1,11 @@
 package com.ccp.implementations.password.mindrot;
 
 import com.ccp.dependency.injection.CcpInstanceProvider;
+import com.ccp.especifications.password.CcpPasswordHandler;
 
-public class CcpMindrotPasswordHandler implements CcpInstanceProvider {
+public class CcpMindrotPasswordHandler implements CcpInstanceProvider<CcpPasswordHandler> {
 
-	
-	public Object getInstance() {
+	public CcpPasswordHandler getInstance() {
 		return new MindrotPasswordHandler();
 	}
-
 }
